d6d3482b5376e3de11aee49357009c1971fb38fa
Rômulo Silva <romulo_lss@hotmail.com>
2022-02-28 10:28:35 -0300

    chore: add lib tesla

--- mix.exs
+++ mix.exs
@@ -43,7 +43,9 @@ defmodule Repoex.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.4.0"},
+      {:hackney, "~> 1.17.0"}
     ]
   end
 
