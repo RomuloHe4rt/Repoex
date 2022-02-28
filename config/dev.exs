aae4bd47398da6da983721be89e0ebabbae4c26e
Rômulo Silva <romulo_lss@hotmail.com>
2022-02-28 11:03:00 -0300

    fix: config/dev

--- config/dev.exs
+++ config/dev.exs
@@ -21,14 +21,6 @@ config :repoex, RepoexWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: []
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
-  check_origin: false,
-  code_reloader: true,
-  debug_errors: true,
-  secret_key_base: "5J/0hI/McknACpid41F6lygVwbbbQi3EEfHxK6F1IfDORVNNErDm8OHK0pdVt/m0",
-  watchers: []
 
 # ## SSL Support
 #
